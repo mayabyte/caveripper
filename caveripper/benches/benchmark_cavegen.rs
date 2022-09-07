2f879e52457c974d61728e65b808bcd37d298c1a
mayabyte <mayahayden@protonmail.com>
2022-09-07 23:08:08 +0100

    Add benchmark for layout rendering

--- caveripper/benches/benchmark_cavegen.rs
+++ caveripper/benches/benchmark_cavegen.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use itertools::Itertools;
 use rand::{Rng, SeedableRng, rngs::SmallRng};
 use caveripper::{
-    layout::Layout,
+    layout::{Layout, render::{render_layout, LayoutRenderOptions}},
     assets::AssetManager,
 };
 
@@ -23,10 +23,28 @@ pub fn benchmark_layout_generation(c: &mut Criterion) {
     });
 }
 
+pub fn benchmark_layout_rendering(c: &mut Criterion) {
+    AssetManager::init_global("assets", ".").unwrap();
+    let mut rng: SmallRng = SeedableRng::seed_from_u64(0x12345678);
+    AssetManager::preload_vanilla_caveinfo()
+        .expect("Failed to load caveinfo!");
+    let manager = AssetManager::all_sublevels().unwrap();
+    let all_sublevels = manager.iter().collect_vec();
+
+    c.bench_function("layout generation + rendering", |b| {
+        b.iter(|| {
+            let seed = rng.gen();
+            let caveinfo = &all_sublevels[rng.gen_range(0..all_sublevels.len())].1;
+            let layout = Layout::generate(seed, caveinfo);
+            black_box(render_layout(&layout, LayoutRenderOptions::default()))
+        })
+    });
+}
+
 
 criterion_group!(
     name = benches;
-    config = Criterion::default().sample_size(10000);
-    targets = benchmark_layout_generation
+    config = Criterion::default().sample_size(1000);
+    targets = benchmark_layout_generation, benchmark_layout_rendering
 );
 criterion_main!(benches);
