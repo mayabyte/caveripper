08d322684e5b98b5d768cf4bcc5c2866ee6f43c0
mayabyte <mayahayden@protonmail.com>
2023-03-13 17:14:34 +0000

    Fix windows install script

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@ ECHO Building Caveripper
 cargo install --path .\cli
 
 ECHO Copying resources folder into $HOME\.config\caveripper\
-Xcopy resources %userprofile%\.config\caveripper
+Xcopy resources %userprofile%\.config\caveripper\ /s /e
 
 ECHO Done. caveripper.exe is located in your cargo/bin directory (usually C:\Users\<Your username>\.cargo\bin)
